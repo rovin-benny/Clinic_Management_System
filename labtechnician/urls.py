a8c4d51eec319ed7c64186df69b43c6dc96a7555
rovin-benny <rovinv@karunya.edu.in>
2025-11-18 14:24:23 +0530

    Lab technician feature added first commit

---
+++ labtechnician/urls.py
@@ -0,0 +1,26 @@
+# In labtechnician/urls.py
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    LabTestCategoryViewSet, 
+    LabTestParameterViewSet, 
+    LabReportViewSet, 
+    LabReportResultViewSet, 
+    LabBillViewSet, 
+    LabBillItemViewSet
+)
+
+# The router automatically creates the URLs for our ViewSets
+router = DefaultRouter()
+router.register(r'test-categories', LabTestCategoryViewSet)
+router.register(r'test-parameters', LabTestParameterViewSet)
+router.register(r'lab-reports', LabReportViewSet)
+router.register(r'lab-report-results', LabReportResultViewSet)
+router.register(r'lab-bills', LabBillViewSet)
+router.register(r'lab-bill-items', LabBillItemViewSet)
+
+# This is the main variable this file exports
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- labtechnician/views.py
+++ labtechnician/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+# --- This is the main change ---
+# We import our new custom permission
+from clinic_admin.permissions import IsLabTechnician
+# ------------------------------
 
-# Create your views here.
+from .models import (
+    LabTestCategory, 
+    LabTestParameter, 
+    LabReport, 
+    LabReportResult, 
+    LabBill, 
+    LabBillItem
+)
+from .serializers import (
+    LabTestCategorySerializer, 
+    LabTestParameterSerializer, 
+    LabReportSerializer, 
+    LabReportResultSerializer, 
+    LabBillSerializer, 
+    LabBillItemSerializer
+)
+
+# --- All ViewSets below are now locked to LabTechnicians only ---
+
+class LabTestCategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsLabTechnician]
+    queryset = LabTestCategory.objects.all()
+    serializer_class = LabTestCategorySerializer
+
+class LabTestParameterViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsLabTechnician]
+    queryset = LabTestParameter.objects.all()
+    serializer_class = LabTestParameterSerializer
+
+class LabReportViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsLabTechnician]
+    queryset = LabReport.objects.all()
+    serializer_class = LabReportSerializer
+
+class LabReportResultViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsLabTechnician]
+    queryset = LabReportResult.objects.all()
+    serializer_class = LabReportResultSerializer
+
+class LabBillViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsLabTechnician]
+    queryset = LabBill.objects.all()
+    serializer_class = LabBillSerializer
+
+class LabBillItemViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsLabTechnician]
+    queryset = LabBillItem.objects.all()
+    serializer_class = LabBillItemSerializer
