c6964511019caee1bed863458119fa990038d8a0
rovin-benny <rovinv@karunya.edu.in>
2025-11-18 12:49:43 +0530

    admin features added first commit

--- clinic_project/urls.py
+++ clinic_project/urls.py
@@ -1,22 +1,31 @@
 """
 URL configuration for clinic_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# --- Import these two views for JWT Login ---
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # --- ADD THESE FOR API LOGIN ---
+    # This creates the URL: /api/token/
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # This creates the URL: /api/token/refresh/
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # --------------------------------
+
+    # --- Your App URLs ---
+    path('api/clinic_admin/', include('clinic_admin.urls')), # Correct
+    path('api/doctor/', include('doctor.urls')),           # <-- ADDED YOUR DOCTOR APP
+    
+    # Your teammates will add their apps here later
+    # path('api/receptionist/', include('receptionist.urls')),
+    path('api/lab/', include('labtechnician.urls')),
+    # path('api/pharmacy/', include('pharmacy.urls')),
 ]
